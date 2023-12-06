b59e4df39ce4579e4f0e28ac74bccf103eb583c6
Ericka Ribeiro <you@example.com>
2023-12-06 15:42:35 -0300

    feat: add file api.ts and add buttons to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { log } from "console";
 import { useState } from "react";
+import{api} from '../../services/api'
 
 const initialItens = [
   { id: 1, nome: "Banana" },
@@ -8,16 +10,49 @@ const initialItens = [
 
 export default function Home() {
   const [itens, setItens] = useState(initialItens);
+  const [textInput, setTextInput] = useState('');
+  const data = {nome: textInput};
 
   async function handleClick() {
-    const response = await fetch("http://192.168.68.154:3000/produtos");
-    const produtos = await response.json();
-    setItens(produtos);
-    console.log(produtos);
+    const response = await api.get("/produtos");
+    console.log(response);
+    setItens(response.data);
+   // const response = await fetch("http://192.168.68.154:3000/produtos");
+    //const produtos = await response.json();
+    //setItens(produtos);
+    //console.log(produtos);
   }
+  async function handleAddItem() {
+    console.log(textInput);
+    console.log("Adicionando um novo item");
+
+    try {
+      const response = await fetch("http://192.168.68.154:3000/produtos", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Ocorreu um erro");
+    }
+  }
+  
 
   return (
     <main>
+      <div style={{marginBottom: 10}}>
+        <input 
+        onChange={(e) => setTextInput(e.target.value)} 
+       
+        type="text" placeholder="Digite seu texto aqui..."/>
+        <button onClick={handleAddItem}>Enviar</button>
+      </div>
       <button onClick={handleClick}>Buscar informação no servidor</button>
 
       <ul>
